e2ca9e2fed1e2e5d238ecebfa34a702666159262
yunsseong <aster20@hanyang.ac.kr>
2024-12-30 02:48:01 +0900

    feat: 랜덤 identifier 생성 기능 구현

---
+++ src/main/java/khakicat/moimserver/member/identifier/IdentifierService.java
@@ -0,0 +1,27 @@
+package khakicat.moimserver.member.identifier;
+
+import java.util.Random;
+import java.util.stream.IntStream;
+import khakicat.moimserver.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class IdentifierService {
+
+    private final MemberRepository memberRepository;
+    private final String stringPool = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    private final int length = 8;
+
+    public String getUniqueIdentifier() {
+        Random random = new Random();
+        StringBuilder identifier = new StringBuilder();
+        do {
+            identifier.delete(0, identifier.length());
+            IntStream.range(0, length)
+                    .forEach(num -> stringPool.charAt(num));
+        } while (memberRepository.findByIdentifier(identifier.toString()).isPresent());
+        return identifier.toString();
+    }
+}

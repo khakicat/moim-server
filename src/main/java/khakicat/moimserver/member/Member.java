346c7e9b0296c4301801e42407b268c816fc3eac
yunsseong <aster20@hanyang.ac.kr>
2024-12-30 02:08:05 +0900

    feat: member 엔티티 구현

---
+++ src/main/java/khakicat/moimserver/member/Member.java
@@ -0,0 +1,29 @@
+package khakicat.moimserver.member;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String email;
+
+    @Column
+    private String nickname;
+
+    @Column
+    private String identifier;
+
+    @Column
+    private String password;
+}

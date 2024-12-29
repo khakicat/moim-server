5869a1c050bbf6a31c01de64f2261140f4c2bb68
yunsseong <aster20@hanyang.ac.kr>
2024-12-30 02:11:32 +0900

    feat: member repository 구현

---
+++ src/main/java/khakicat/moimserver/member/MemberRepository.java
@@ -0,0 +1,11 @@
+package khakicat.moimserver.member;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
+    Optional<Member> findByIdentifier(String identifier);
+}

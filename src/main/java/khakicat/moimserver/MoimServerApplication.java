6d418eee75bac4d8eb4938fa8b27deb1b73b2e2b
yunsseong <aster20@hanyang.ac.kr>
2024-12-29 23:19:17 +0900

    chore: 프로젝트 환경설정

---
+++ src/main/java/khakicat/moimserver/MoimServerApplication.java
@@ -0,0 +1,13 @@
+package khakicat.moimserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoimServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoimServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/khakicat/moimserver/MoimServerApplicationTests.java
@@ -0,0 +1,13 @@
+package khakicat.moimserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoimServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

a07af8efc09e78a4b590a441c6243df9a18566f5
yunsseong <aster20@hanyang.ac.kr>
2024-12-30 02:14:48 +0900

    feat: 다중 id(email, identifier) 기반 사용자 탐색 기능 구현

---
+++ src/main/java/khakicat/moimserver/security/service/PrincipalDetailService.java
@@ -0,0 +1,44 @@
+package khakicat.moimserver.security.service;
+
+import java.util.Optional;
+import khakicat.moimserver.member.Member;
+import khakicat.moimserver.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PrincipalDetailService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        if (username.contains("@")) {
+            return userBuilder(findMemberByEmail(username));
+        }
+        return userBuilder(findMemberByIdentifier(username));
+    }
+
+    public Member findMemberByEmail(String email) throws UsernameNotFoundException {
+        return memberRepository
+                .findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("유저를 찾을 수 없습니다."));
+    }
+
+    public Member findMemberByIdentifier(String identifier) throws UsernameNotFoundException {
+        return memberRepository
+                .findByIdentifier(identifier)
+                .orElseThrow(() -> new UsernameNotFoundException("유저를 찾을 수 없습니다."));
+    }
+
+    public UserDetails userBuilder(Member member) {
+        return User.builder()
+                .username(member.getIdentifier())
+                .password(member.getPassword())
+                .build();
+    }
+}

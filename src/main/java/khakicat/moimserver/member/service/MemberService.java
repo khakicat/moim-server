e55466ac3b5d94e73f89a7e040621f24e9811550
yunsseong <aster20@hanyang.ac.kr>
2024-12-30 22:17:39 +0900

    feat: member 추가 기능 구현

---
+++ src/main/java/khakicat/moimserver/member/service/MemberService.java
@@ -0,0 +1,24 @@
+package khakicat.moimserver.member.service;
+
+import khakicat.moimserver.member.dto.MemberDTO;
+import khakicat.moimserver.member.model.Member;
+import khakicat.moimserver.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+    private final IdentifierService identifierService;
+
+    public Member addMember(MemberDTO memberDTO) {
+        return memberRepository.save(new Member(
+                memberDTO.getEmail(),
+                memberDTO.getNickname(),
+                identifierService.getUniqueIdentifier(),
+                memberDTO.getPassword()
+        ));
+    }
+}

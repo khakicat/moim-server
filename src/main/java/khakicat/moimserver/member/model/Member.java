9b5e08a3226fadbfb514ea267c79b2fd884fc0f5
yunsseong <aster20@hanyang.ac.kr>
2025-01-06 11:45:45 +0900

    fix: 기본 생성자 누락 수정

--- src/main/java/khakicat/moimserver/member/model/Member.java
+++ src/main/java/khakicat/moimserver/member/model/Member.java
@@ -6,9 +6,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@NoArgsConstructor
 public class Member {
 
     @Id

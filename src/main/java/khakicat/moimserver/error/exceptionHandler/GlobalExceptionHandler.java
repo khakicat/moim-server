801d58a381daa3cae75a9eeafca6388f26043526
yunsseong <aster20@hanyang.ac.kr>
2025-01-06 16:06:06 +0900

    feat: 예외 처리 단순화

--- src/main/java/khakicat/moimserver/error/exceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/khakicat/moimserver/error/exceptionHandler/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package khakicat.moimserver.error.exceptionHandler;
 
 import khakicat.moimserver.error.response.ErrorResponse;
+import khakicat.moimserver.error.response.ErrorResponses;
 import khakicat.moimserver.error.exception.MultipleErrorsException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +12,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MultipleErrorsException.class)
-    public ResponseEntity<ErrorResponse> handleMultipleErrorsException(MultipleErrorsException ex) {
-        return new ResponseEntity<>(new ErrorResponse(ex.getErrorMessages()), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<ErrorResponses> handleMultipleErrorsException(MultipleErrorsException ex) {
+        return new ResponseEntity<>(new ErrorResponses(ex.getErrorMessages()), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception ex) {
+        return new ResponseEntity<>(new ErrorResponse(ex.getMessage()), HttpStatus.UNAUTHORIZED);
     }
 }

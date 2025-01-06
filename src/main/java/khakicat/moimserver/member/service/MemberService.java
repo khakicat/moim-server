ab9296b6371b269907dc45184ef66f3cc54622e7
yunsseong <aster20@hanyang.ac.kr>
2025-01-06 15:52:58 +0900

    feat: 비밀번호 암호화 기능 구현

--- src/main/java/khakicat/moimserver/member/service/MemberService.java
+++ src/main/java/khakicat/moimserver/member/service/MemberService.java
@@ -4,6 +4,8 @@ import khakicat.moimserver.member.dto.MemberDTO;
 import khakicat.moimserver.member.model.Member;
 import khakicat.moimserver.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,14 +14,15 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
     private final IdentifierService identifierService;
+    private final PasswordEncoder passwordEncoder;
 
     public Member addMember(MemberDTO memberDTO) {
         return memberRepository.save(new Member(
                 memberDTO.getEmail(),
                 memberDTO.getNickname(),
                 identifierService.getUniqueIdentifier(),
-                memberDTO.getPassword()
-        ));
+                passwordEncoder.encode(memberDTO.getPassword()))
+        );
     }
 
     public boolean isEmailAlreadyExist(String email) {

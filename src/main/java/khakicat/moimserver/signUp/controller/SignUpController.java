2cc909be6323ad638c1dbc7af855503d581259ad
yunsseong <aster20@hanyang.ac.kr>
2025-01-06 11:46:50 +0900

    feat: 회원가입 반환값 변경

--- src/main/java/khakicat/moimserver/signUp/controller/SignUpController.java
+++ src/main/java/khakicat/moimserver/signUp/controller/SignUpController.java
@@ -1,9 +1,9 @@
 package khakicat.moimserver.signUp.controller;
 
-import khakicat.moimserver.member.model.Member;
 import khakicat.moimserver.signUp.service.SignUpService;
 import khakicat.moimserver.signUp.signupDTO.SignUpDTO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,7 +18,8 @@ public class SignUpController {
     private final SignUpService signUpService;
 
     @PostMapping
-    public ResponseEntity<Member> signUp(@RequestBody SignUpDTO signUpDTO) {
-        return ResponseEntity.ok(signUpService.signUp(signUpDTO));
+    public ResponseEntity<String> signUp(@RequestBody SignUpDTO signUpDTO) {
+        signUpService.signUp(signUpDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     };
 }

81a4747a2261f9c315267f1a692c11592ffd5b9b
yunsseong <aster20@hanyang.ac.kr>
2025-01-06 11:46:01 +0900

    chore: 불필요한 어노테이션 삭제

--- src/main/java/khakicat/moimserver/member/dto/MemberDTO.java
+++ src/main/java/khakicat/moimserver/member/dto/MemberDTO.java
@@ -1,10 +1,8 @@
 package khakicat.moimserver.member.dto;
 
-import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-@Builder
 public class MemberDTO {
     private String email;
     private String nickname;
